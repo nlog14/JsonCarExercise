2fef36eadfc9a3170af8f4faa38c831a354281d0
Nohely <ngedeon14@yahoo.com>
2021-09-14 10:59:57 +0200

    Add project files.

---
+++ JsonCarExercise/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace JsonCarExercise
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string registrationNumber { get; set; }
+
+        public Car()
+        {
+
+        }
+
+
+        public Car(string Model, string Color, string registrationNumber)
+        {
+            this.Model = Model;
+            this.Color = Color;
+            this.registrationNumber = registrationNumber;
+        }
+    }
+}
---
+++ JsonCarExercise/CarDealerShip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonCarExercise
+{
+    class CarDealerShip
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        
+        public List<Car> ListOfCars { get; set; }
+    }
+}
